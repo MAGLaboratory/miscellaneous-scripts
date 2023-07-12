b0d08b1681e57a0d9f975dc9f88ba28f9b899aea
Brandon Kirisaki <b.k.lu@ieee.org>
2023-07-11 20:12:52 -0700

    Modified power line test script.

--- modbus_test/power_line_test.py
+++ modbus_test/power_line_test.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 import minimalmodbus
-import time, signal
+import datetime, time, signal
 
 p_exit = 0
 
@@ -10,8 +12,19 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+# settings
+testing_count = 10000000
+progress = False
+pnt_time = True
+sleep_wait = 0.0
 
-for target_timeout in [0.22, 0.23, 0.24, 0.25]:
+timeout_list = []
+
+for i in range(3):
+    timeout_list.append(i * 0.005 + 0.395)
+
+
+for target_timeout in timeout_list:
     succ = 0
     fail = 0
     c_co = 0
@@ -23,18 +36,21 @@ for target_timeout in [0.22, 0.23, 0.24, 0.25]:
     instr.serial.baudrate = 38400
     instr.serial.timeout = target_timeout
     print("Timeout: " + str(instr.serial.timeout))
-    
-    # i = 0
-    
+    if sleep_wait > 0:
+        print("Sleep Wait: " + str(sleep_wait))
     # while (not p_exit):
-    for i in range(1000000):
+    if pnt_time:
+        start_time = datetime.datetime.now()
+    for i in range(testing_count):
         try:
             dummy = instr.read_register(0x00, functioncode=4)
             c_co = 0
             succ += 1
-            # print(".", end = '', flush=True)
+            if progress:
+                print(".", end = '', flush=True)
         except IOError as err:
-            # print("x", end = '', flush=Truae)
+            if progress:
+                print("x", end = '', flush=True)
             if e_counter < 10:
                 errors += str(err)
                 errors += "\n"
@@ -43,16 +59,20 @@ for target_timeout in [0.22, 0.23, 0.24, 0.25]:
             if c_co > cons:
                 cons = c_co
             fail += 1
-        #if (i % 80 == 79):
-        #    print("")
-        #time.sleep(0.1)
-        #i += 1
+        if progress and (i % 80 == 79):
+            print("")
+        if sleep_wait > 0.0:
+            time.sleep(0.1)
         if p_exit:
             break
     
     print()
+    if pnt_time:
+        print("Execution time: {}".format(datetime.datetime.now() - start_time))
     print("Success: " + str(succ), ", Failure: " + str(fail), end='')
     print(", Consecutive Failures: " + str(cons))
     print("Exceptions encountered:")
     print(errors)
+    if p_exit:
+        break
     
