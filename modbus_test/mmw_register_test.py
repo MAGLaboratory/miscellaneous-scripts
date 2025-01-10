207fb40c26b83e9ab3424be5f341dc7a2ddb1456
Brandon <blu006@ucr.edu>
2025-01-09 18:35:36 -0800

    modbug register test

---
+++ modbus_test/mmw_register_test.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3 
+
+import minimalmodbus
+import time
+
+my_16bit_registers = 0x00
+my_16bit_data = [0x1234, 0x5678, 0xABCD, 0xEF12]
+
+my_32bit_data = {0x04: 0xAF12345A, 0x06: 0x005678FF}
+
+my_64bit_data = {0x08: 0xA5123456789AA500}
+
+instr = minimalmodbus.Instrument("COM4", 255)
+instr.address = 255
+instr.serial.baudrate = 38400
+instr.serial.timeout = 0.5
+
+print("Checking communication.")
+
+# check communication
+try:
+    print(instr.read_registers(0x00, 1))
+except:
+    print("Communication failed.")
+    exit();
+
+time.sleep(1);
+
+print("Writing 16-bit registers")
+instr.write_registers(my_16bit_registers, my_16bit_data)
+
+time.sleep(1);
+
+print("Writing 32-bit registers")
+for addr, data in my_32bit_data.items():
+    instr.write_long(addr, data)
+
+time.sleep(1);
+
+print("Writing 64-bit registers")
+instr.write_long(0x08, my_64bit_data[0x08], number_of_registers=4)
+
+time.sleep(1);
+
+print("Now checking registers...")
+
+time.sleep(1);
+print("Reading 16-bit registers")
+tmp_16bit_data = instr.read_registers(0x00, 4)
+print(tmp_16bit_data)
+assert (tmp_16bit_data == my_16bit_data)
+
+time.sleep(1);
+print("Reading 32-bit registers")
+tmp_32bit_data = {}
+for addr in my_32bit_data.keys():
+    tmp_32bit_data.update({addr: instr.read_long(addr)})
+
+assert (tmp_32bit_data == my_32bit_data)
+
+time.sleep(1);
+print("Reading 64-bit registers")
+tmp_64bit_data = {}
+for addr in my_64bit_data.keys():
+    tmp_64bit_data.update({addr: instr.read_long(addr, number_of_registers=4)})
+
+assert (tmp_64bit_data == my_64bit_data)
+
+time.sleep(1);
+
+print("Success!")
